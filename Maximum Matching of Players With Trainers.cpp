7916931adee90790416b900aaae17c0211bc10f0
Nilay Badjatya <147936252+NilayBadjatya@users.noreply.github.com>
2025-07-13 11:13:17 +0530

    Create Maximum Matching of Players With Trainers.cpp

---
+++ Maximum Matching of Players With Trainers.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int matchPlayersAndTrainers(vector<int>& players, vector<int>& trainers) {
+        sort(players.begin(), players.end());
+        sort(trainers.begin(), trainers.end());
+        int n = players.size();
+        int m = trainers.size();
+        int i = 0, j = 0, matchings = 0;
+        while(i < n && j < m){
+            if(players[i] <= trainers[j]){
+                matchings++;
+                i++;
+                j++;
+            }else{
+                j++;
+            }
+        }
+        return matchings;
+    }
+};
